827897dff7194a34c4c0dca3e3cc6d10892ccfe5
Rathin21 <114067851+Rathin21@users.noreply.github.com>
2023-03-14 16:34:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+
+function taskfnc(event){
+	event.preventDefault();
+	let Age = document.querySelector("#age").value;
+	let Name = document.querySelector("#name").value;
+	
+	let prom =  new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(Age>=18){
+				resolve(`Welcome, ${Name}. You can vote.`);
+			}
+			else{
+				reject(`Oh sorry ${Name}. You aren't old enough.`);
+			}
+		},4000);
+		
+	}); 
+
+	prom.then((data)=>alert(data)).catch((data)=>alert(data));
+}
